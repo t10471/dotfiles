09b80324c1e254f291f229a69041f70202a15736
t10471 <t104711202@gmail.com>
2014-10-14 05:55:35 +0900

    added screen.sh

--- link.sh
+++ link.sh
@@ -5,6 +5,5 @@ ln -s ${PWD}/_vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
-wget https://gist.githubusercontent.com/w0ng/3278077/raw/073a61b47ccfea8a7ea30b5aabde3f3acc09ca03/.Xresources-hybrid -O ~/.Xresources
-
+bash screen.sh
 bash scala.sh
--- unlink.sh
+++ unlink.sh
@@ -2,3 +2,5 @@
 rm ~/.screenrc
 rm ~/.vimrc
 rm -rf ~/.vim/bundle
+rm -rf ~/.Xresources
+
