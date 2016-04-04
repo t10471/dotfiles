90023eb0bb2bf1f90b23cb27e7dfc08eee6712a6
t10471 <t104711202@gmail.com>
2016-04-04 15:52:49 +0900

    update for mac

--- link.sh
+++ link.sh
@@ -2,11 +2,22 @@
 PWD=$(cd $(dirname $0) && pwd)
 ln -s ${PWD}/_screenrc ~/.screenrc
 ln -s ${PWD}/_vimrc ~/.vimrc
-ln -s ${PWD}/_vimshrc ~/.vimshrc
+
+case "${OSTYPE}" in
+darwin*)
+  ln -s ${PWD}/_vimshrc_mac ~/.vimshrc
+  ;;
+linux*)
+  ln -s ${PWD}/_vimshrc_linux ~/.vimshrc
+  ;;
+esac
+
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
 ln -s ${PWD}/after ~/.vim/after
 bash screen.sh
 bash scala.sh
+mkdir -p ~/.vim/snippets
 bash snip.sh
+mkdir -p ~/.vim/tmp
