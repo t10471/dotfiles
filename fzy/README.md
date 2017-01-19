ee7f315d66b7a1e83ac5b2c5df4a70920934f09c
t10471 <t104711202@gmail.com>
2017-01-19 13:26:13 +0900

    up

---
+++ fzy/README.md
@@ -0,0 +1,110 @@
+![fzy](http://i.hawth.ca/u/fzy-github.svg)
+
+**fzy** is a fast, simple fuzzy text selector for the terminal with an advanced [scoring
+algorithm](#sorting).
+
+![](http://i.hawth.ca/u/fzy2.gif)
+
+<blockquote>
+It's been kind of life-changing.
+-<a href="https://github.com/graygilmore/">@graygilmore</a>
+</blockquote>
+
+<blockquote>
+fzy works great btw
+-<a href="https://twitter.com/alexblackie/status/719297828892188672">@alexblackie</a>
+</blockquote>
+
+[![Build Status](https://travis-ci.org/jhawthorn/fzy.svg?branch=master)](https://travis-ci.org/jhawthorn/fzy)
+
+## Why use this over fzf, pick, selecta, ctrlp, ...?
+
+fzy is faster and shows better results than other fuzzy finders.
+
+Most other fuzzy matchers sort based on the length of a match. fzy tries to
+find the result the user intended. It does this by favouring matches on
+consecutive letters and starts of words. This allows matching using acronyms or
+different parts of the path.
+
+A gory comparison of the sorting used by fuzzy finders can be found in [ALGORITHM.md](ALGORITHM.md)
+
+fzy is designed to be used both as an editor plugin and on the command line.
+Rather than clearing the screen, fzy displays its interface directly below the current cursor position, scrolling the screen if necessary.
+
+## Installation
+
+### From source
+
+    make
+    sudo make install
+
+The `PREFIX` environment variable can be used to specify the install location,
+the default is `/usr/local`.
+
+### Ubuntu/Debian 64-bit
+
+    wget https://github.com/jhawthorn/fzy/releases/download/0.8/fzy_0.8-1_amd64.deb
+    sudo dpkg -i fzy_0.8-1_amd64.deb
+
+### Fedora/Redhat/CentOS
+
+    sudo yum install https://github.com/jhawthorn/fzy/releases/download/0.8/fzy-0.8-1.x86_64.rpm
+
+### Arch Linux
+
+fzy is available
+[in the AUR](https://aur.archlinux.org/packages/fzy/)
+with a very simple
+[PKGBUILD](https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=fzy)
+
+### OS X via Homebrew
+
+    brew tap jhawthorn/fzy
+    brew install fzy
+
+## Usage
+
+fzy is a drop in replacement for [selecta](https://github.com/garybernhardt/selecta), and can be used with its [usage examples](https://github.com/garybernhardt/selecta#usage-examples).
+
+### Use with Vim
+
+fzy can be integrated very simply in vim. There is also [fzy-vim](https://github.com/Dkendal/fzy-vim), a more fully featured vim plugin.
+
+``` vim
+function! FzyCommand(choice_command, vim_command)
+  try
+    let output = system(a:choice_command . " | fzy ")
+  catch /Vim:Interrupt/
+    " Swallow errors from ^C, allow redraw! below
+  endtry
+  redraw!
+  if v:shell_error == 0 && !empty(output)
+    exec a:vim_command . ' ' . output
+  endif
+endfunction
+
+nnoremap <leader>e :call FzyCommand("find -type f", ":e")<cr>
+nnoremap <leader>v :call FzyCommand("find -type f", ":vs")<cr>
+nnoremap <leader>s :call FzyCommand("find -type f", ":sp")<cr>
+```
+
+Any program can be used to filter files presented through fzy. [ag (the silver searcher)](https://github.com/ggreer/the_silver_searcher) can be used to ignore files specified by `.gitignore`.
+
+``` vim
+nnoremap <leader>e :call FzyCommand("ag . -l -g ''", ":e")<cr>
+nnoremap <leader>v :call FzyCommand("ag . -l -g ''", ":vs")<cr>
+nnoremap <leader>s :call FzyCommand("ag . -l -g ''", ":sp")<cr>
+```
+
+## Sorting
+
+fzy attempts to present the best matches first. The following considerations are weighted when sorting:
+
+It prefers consecutive characters: `file` will match <tt><b>file</b></tt> over <tt><b>fil</b>t<b>e</b>r</tt>.
+
+It prefers matching the beginning of words: `amp` is likely to match <tt><b>a</b>pp/<b>m</b>odels/<b>p</b>osts.rb</tt>.
+
+It prefers shorter matches: `abce` matches <tt><b>abc</b>d<b>e</b>f</tt> over <tt><b>abc</b> d<b>e</b></tt>.
+
+It prefers shorter candidates: `test` matches <tt><b>test</b>s</tt> over <tt><b>test</b>ing</b></tt>.
+
---
+++ fzy/test/fzytest.c
@@ -0,0 +1,318 @@
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+
+#include "../config.h"
+#include "match.h"
+#include "choices.h"
+
+int testsrun = 0, testsfailed = 0, assertionsrun = 0;
+
+#define assert(x)                                                                                  \
+	if (++assertionsrun && !(x)) {                                                             \
+		fprintf(stderr, "test \"%s\" failed\n   assert(%s) was false\n   at %s:%i\n\n",    \
+			__func__, #x, __FILE__, __LINE__);                                         \
+		raise(SIGTRAP);                                                                    \
+		testsfailed++;                                                                     \
+		return;                                                                            \
+	}
+
+#define assert_streq(a, b) assert(!strcmp(a, b))
+
+void runtest(void (*test)()) {
+	testsrun++;
+	test();
+}
+
+void test_match() {
+	assert(has_match("a", "a"));
+	assert(has_match("a", "ab"));
+	assert(has_match("a", "ba"));
+	assert(has_match("abc", "a|b|c"));
+
+	/* non-match */
+	assert(!has_match("a", ""));
+	assert(!has_match("a", "b"));
+	assert(!has_match("ass", "tags"));
+
+	/* match when query is empty */
+	assert(has_match("", ""));
+	assert(has_match("", "a"));
+}
+
+void test_relative_scores() {
+	/* App/Models/Order is better than App/MOdels/zRder  */
+	assert(match("amor", "app/models/order") > match("amor", "app/models/zrder"));
+
+	/* App/MOdels/foo is better than App/M/fOo  */
+	assert(match("amo", "app/m/foo") < match("amo", "app/models/foo"));
+
+	/* GEMFIle.Lock < GEMFILe  */
+	assert(match("gemfil", "Gemfile.lock") < match("gemfil", "Gemfile"));
+
+	/* GEMFIle.Lock < GEMFILe  */
+	assert(match("gemfil", "Gemfile.lock") < match("gemfil", "Gemfile"));
+
+	/* Prefer shorter matches */
+	assert(match("abce", "abcdef") > match("abce", "abc de"));
+
+	/* Prefer shorter candidates */
+	assert(match("test", "tests") > match("test", "testing"));
+
+	/* Scores first letter highly */
+	assert(match("test", "testing") > match("test", "/testing"));
+
+	/* Prefer shorter matches */
+	assert(match("abc", "    a b c ") > match("abc", " a  b  c "));
+	assert(match("abc", " a b c    ") > match("abc", " a  b  c "));
+}
+
+void test_exact_scores() {
+	/* Exact match is SCORE_MAX */
+	assert(match("abc", "abc") == SCORE_MAX);
+	assert(match("aBc", "abC") == SCORE_MAX);
+
+	/* Empty query always results in SCORE_MIN */
+	assert(match("", "") == SCORE_MIN);
+	assert(match("", "a") == SCORE_MIN);
+	assert(match("", "bb") == SCORE_MIN);
+
+	/* Gaps */
+	assert(match("a", "*a") == SCORE_GAP_LEADING);
+	assert(match("a", "*ba") == SCORE_GAP_LEADING*2);
+	assert(match("a", "**a*") == SCORE_GAP_LEADING*2 + SCORE_GAP_TRAILING);
+	assert(match("a", "**a**") == SCORE_GAP_LEADING*2 + SCORE_GAP_TRAILING*2);
+	assert(match("aa", "**aa**") == SCORE_GAP_LEADING*2 + SCORE_MATCH_CONSECUTIVE + SCORE_GAP_TRAILING*2);
+	assert(match("aa", "**a*a**") == SCORE_GAP_LEADING + SCORE_GAP_LEADING + SCORE_GAP_INNER + SCORE_GAP_TRAILING + SCORE_GAP_TRAILING);
+
+	/* Consecutive */
+	assert(match("aa", "*aa") == SCORE_GAP_LEADING + SCORE_MATCH_CONSECUTIVE);
+	assert(match("aaa", "*aaa") == SCORE_GAP_LEADING + SCORE_MATCH_CONSECUTIVE*2);
+	assert(match("aaa", "*a*aa") == SCORE_GAP_LEADING + SCORE_GAP_INNER + SCORE_MATCH_CONSECUTIVE);
+
+	/* Slash */
+	assert(match("a", "/a") == SCORE_GAP_LEADING + SCORE_MATCH_SLASH);
+	assert(match("a", "*/a") == SCORE_GAP_LEADING*2 + SCORE_MATCH_SLASH);
+	assert(match("aa", "a/aa") == SCORE_GAP_LEADING*2 + SCORE_MATCH_SLASH + SCORE_MATCH_CONSECUTIVE);
+
+	/* Capital */
+	assert(match("a", "bA") == SCORE_GAP_LEADING + SCORE_MATCH_CAPITAL);
+	assert(match("a", "baA") == SCORE_GAP_LEADING*2 + SCORE_MATCH_CAPITAL);
+	assert(match("aa", "baAa") == SCORE_GAP_LEADING*2 + SCORE_MATCH_CAPITAL + SCORE_MATCH_CONSECUTIVE);
+
+	/* Dot */
+	assert(match("a", ".a") == SCORE_GAP_LEADING + SCORE_MATCH_DOT);
+	assert(match("a", "*a.a") == SCORE_GAP_LEADING*3 + SCORE_MATCH_DOT);
+	assert(match("a", "*a.a") == SCORE_GAP_LEADING + SCORE_GAP_INNER + SCORE_MATCH_DOT);
+}
+
+void test_positions_1() {
+	size_t positions[3];
+	match_positions("amo", "app/models/foo", positions);
+	assert(positions[0] == 0);
+	assert(positions[1] == 4);
+	assert(positions[2] == 5);
+}
+
+void test_positions_2() {
+	/*
+	 * We should prefer matching the 'o' in order, since it's the beginning
+	 * of a word.
+	 */
+	size_t positions[4];
+	match_positions("amor", "app/models/order", positions);
+	assert(positions[0] == 0);
+	assert(positions[1] == 4);
+	assert(positions[2] == 11);
+}
+
+void test_positions_3() {
+	size_t positions[2];
+	match_positions("as", "tags", positions);
+	assert(positions[0] == 1);
+	assert(positions[1] == 3);
+}
+
+void test_positions_4() {
+	size_t positions[2];
+	match_positions("as", "examples.txt", positions);
+	assert(positions[0] == 2);
+	assert(positions[1] == 7);
+}
+
+void test_positions_5() {
+	size_t positions[3];
+	match_positions("abc", "a/a/b/c/c", positions);
+	assert(positions[0] == 2);
+	assert(positions[1] == 4);
+	assert(positions[2] == 6);
+}
+
+void test_positions_exact() {
+	size_t positions[3];
+	match_positions("foo", "foo", positions);
+	assert(positions[0] == 0);
+	assert(positions[1] == 1);
+	assert(positions[2] == 2);
+}
+
+void test_choices_empty() {
+	choices_t choices;
+	choices_init(&choices);
+	assert(choices.size == 0);
+	assert(choices.available == 0);
+	assert(choices.selection == 0);
+
+	choices_prev(&choices);
+	assert(choices.selection == 0);
+
+	choices_next(&choices);
+	assert(choices.selection == 0);
+
+	choices_destroy(&choices);
+}
+
+void test_choices_1() {
+	choices_t choices;
+	choices_init(&choices);
+	choices_add(&choices, "tags");
+
+	choices_search(&choices, "");
+	assert(choices.available == 1);
+	assert(choices.selection == 0);
+
+	choices_search(&choices, "t");
+	assert(choices.available == 1);
+	assert(choices.selection == 0);
+
+	choices_prev(&choices);
+	assert(choices.selection == 0);
+
+	choices_next(&choices);
+	assert(choices.selection == 0);
+
+	assert(!strcmp(choices_get(&choices, 0), "tags"));
+	assert(choices_get(&choices, 1) == NULL);
+
+	choices_destroy(&choices);
+}
+
+void test_choices_2() {
+	choices_t choices;
+	choices_init(&choices);
+	choices_add(&choices, "tags");
+	choices_add(&choices, "test");
+
+	/* Empty search */
+	choices_search(&choices, "");
+	assert(choices.selection == 0);
+	assert(choices.available == 2);
+	assert_streq(choices_get(&choices, 0), "tags");
+	assert_streq(choices_get(&choices, 1), "test");
+
+	choices_next(&choices);
+	assert(choices.selection == 1);
+	choices_next(&choices);
+	assert(choices.selection == 0);
+
+	choices_prev(&choices);
+	assert(choices.selection == 1);
+	choices_prev(&choices);
+	assert(choices.selection == 0);
+
+	/* Filtered search */
+	choices_search(&choices, "te");
+	assert(choices.available == 1);
+	assert(choices.selection == 0);
+	assert_streq(choices_get(&choices, 0), "test");
+
+	choices_next(&choices);
+	assert(choices.selection == 0);
+
+	choices_prev(&choices);
+	assert(choices.selection == 0);
+
+	/* No results */
+	choices_search(&choices, "foobar");
+	assert(choices.available == 0);
+	assert(choices.selection == 0);
+
+	/* Different order due to scoring */
+	choices_search(&choices, "ts");
+	assert(choices.available == 2);
+	assert(choices.selection == 0);
+	assert_streq(choices_get(&choices, 0), "test");
+	assert_streq(choices_get(&choices, 1), "tags");
+
+	choices_destroy(&choices);
+}
+
+void test_choices_without_search() {
+	/* Before a search is run, it should return no results */
+
+	choices_t choices;
+	choices_init(&choices);
+
+	assert(choices.available == 0);
+	assert(choices.selection == 0);
+	assert(choices.size == 0);
+	assert(choices_get(&choices, 0) == NULL);
+
+	choices_add(&choices, "test");
+
+	assert(choices.available == 0);
+	assert(choices.selection == 0);
+	assert(choices.size == 1);
+	assert(choices_get(&choices, 0) == NULL);
+
+	choices_destroy(&choices);
+}
+
+/* Regression test for segfault */
+void test_choices_unicode() {
+	choices_t choices;
+	choices_init(&choices);
+
+	choices_add(&choices, "Edmund Husserl - Méditations cartésiennes - Introduction a la phénoménologie.pdf");
+	choices_search(&choices, "e");
+
+	choices_destroy(&choices);
+}
+
+void summary() {
+	printf("%i tests, %i assertions, %i failures\n", testsrun, assertionsrun, testsfailed);
+}
+
+static void ignore_signal(int signum) {
+	(void)signum;
+}
+
+int main(int argc, char *argv[]) {
+	(void)argc;
+	(void)argv;
+
+	/* We raise sigtrap on all assertion failures.
+	 * If we have no debugger running, we should ignore it */
+	signal(SIGTRAP, ignore_signal);
+
+	runtest(test_match);
+	runtest(test_relative_scores);
+	runtest(test_exact_scores);
+	runtest(test_positions_1);
+	runtest(test_positions_2);
+	runtest(test_positions_3);
+	runtest(test_positions_4);
+	runtest(test_positions_5);
+	runtest(test_positions_exact);
+
+	runtest(test_choices_empty);
+	runtest(test_choices_1);
+	runtest(test_choices_2);
+	runtest(test_choices_without_search);
+	runtest(test_choices_unicode);
+
+	summary();
+
+	/* exit 0 if all tests pass */
+	return !!testsfailed;
+}
