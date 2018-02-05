e1b74255d9247d7df50127633c36f8d75b17cb77
t10471 <t104711202@gmail.com>
2018-02-06 05:42:04 +0900

    update

---
+++ global.sh
@@ -0,0 +1,11 @@
+# brew install global --with-exuberant-ctags --with-pygments
+# cp /usr/local/Cellar/global/6.6.1/share/gtags/gtags.conf ~/dotfiles/gtags.conf
+
+# gtags.confの下を
+# default:\
+#    :tc=native:
+
+# default:\
+#    :tc=native:tc=pygments:
+
+cp gtags.conf $HOME/.globalrc
