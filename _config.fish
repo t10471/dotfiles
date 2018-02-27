30bd530c2e1d0d4db611672f28b6c721d3fb1903
t10471 <t104711202@gmail.com>
2018-02-27 17:11:18 +0900

    update

--- _config.fish
+++ _config.fish
@@ -60,37 +60,7 @@ end
 set fish_plugins theme peco z tmux goenv debug
 
 function fish_prompt
-  if test -z $VIMRUNTIME
-    ~/.pyenv/versions/anaconda3-2.5.0/bin/python ~/powerline-shell/powerline-shell.py $status --shell bare ^/dev/null
-  else
-    vim_fish_prompt
-  end
-end
-
-function vim_fish_prompt -d "Prints left prompt"
-  set -l last_status  $status
-  set -l glyph        "\$"
-  set -l glyph_color  (set_color normal)
-  set -l pwd          (prompt_pwd)
-  set -l pwd_color    (set_color blue)
-
-  if test (id -u "$USER") -eq 0
-    set glyph "#"
-  end
-
-  if test "$last_status" -ne 0
-    set pwd_color (set_color red)
-  end
-
-  if git_is_repo
-    if git_is_staged
-      set glyph_color (set_color green)
-    else if git_is_stashed
-      set glyph_color (set_color yellow)
-    end
-  end
-
-    printf " $pwd_color$pwd $glyph_color$glyph "
+    powerline-shell --shell bare $status
 end
 
 function peco
