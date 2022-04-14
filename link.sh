8563ce475abfce00b749a3ee3d8bb36765540eb4
t10471 <t104711202@gmail.com>
2022-04-14 10:27:54 +0900

    update

--- link.sh
+++ link.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 PWD=$(cd $(dirname $0) && pwd)
-mkdir -p ~/.config/nvim
-ln -s ${PWD}/_eslintrc.json ~/.eslintrc
-ln -s ${PWD}/_vimrc ~/.vimrc
+# mkdir -p ~/.config/nvim
+# ln -s ${PWD}/_eslintrc.json ~/.eslintrc
+# ln -s ${PWD}/_vimrc ~/.vimrc
 ln -s ${PWD}/_vimrc ~/.config/nvim/init.vim
-ln -s ${PWD}/_tmux.conf ~/.tmux.conf
-# ln -s ${PWD}/_config.fish ~/.config/fish/config.fish
-ln -s ${PWD}/_zshrc ~/.zshrc
-ln -s ${PWD}/_powerline-shell.json ~/.powerline-shell.json
-mkdir -p ~/.vim.after
-ln -s ${PWD}/after ~/.vim/after
-bash link_karabiner.sh
-mkdir -p ~/.vim/snippets
-bash snip.sh
-mkdir -p ~/.vim/tmp
-mkdir -p ~/.cache/dein
+# ln -s ${PWD}/_tmux.conf ~/.tmux.conf
+# # ln -s ${PWD}/_config.fish ~/.config/fish/config.fish
+# ln -s ${PWD}/_zshrc ~/.zshrc
+# ln -s ${PWD}/_powerline-shell.json ~/.powerline-shell.json
+# ln -s ${PWD}/.path.sh ~/.path.sh
+# ln -s ${PWD}/tigrc ~/.tigrc
+# mkdir -p ~/.vim.after
+# ln -s ${PWD}/after ~/.vim/after
+# bash link_karabiner.sh
+# mkdir -p ~/.vim/snippets
+# bash snip.sh
+# mkdir -p ~/.vim/tmp
+# mkdir -p ~/.cache/dein
