c1b1d200db55f13b43977b4aeaeca681bd8e5f72
t10471 <t104711202@gmail.com>
2022-04-22 14:25:11 +0900

    update

--- link.sh
+++ link.sh
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 PWD=$(cd $(dirname $0) && pwd)
 # mkdir -p ~/.config/nvim
 # ln -s ${PWD}/_eslintrc.json ~/.eslintrc
 # ln -s ${PWD}/_vimrc ~/.vimrc
-ln -s ${PWD}/_vimrc ~/.config/nvim/init.vim
+# ln -s ${PWD}/_vimrc ~/.config/nvim/init.vim
 # ln -s ${PWD}/_tmux.conf ~/.tmux.conf
 # # ln -s ${PWD}/_config.fish ~/.config/fish/config.fish
 # ln -s ${PWD}/_zshrc ~/.zshrc
